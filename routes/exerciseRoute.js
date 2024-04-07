0d1ea15761d12b6624a33c30c41ae15fcc740819
Davi <daviregonatti@gmail.com>
2024-04-07 03:20:19 -0300

    feat: created route file
    created route file for the exercises

---
+++ routes/exerciseRoute.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const exerciseController = require("../controllers/exerciseController");
+
+const router = express.Router();
+
+router.route("/").get("/", exerciseController.getAllExercises);
+
+router
+  .route("/:id")
+  .get(exerciseController.getExercise)
+  .patch(exerciseController.updateExercise);
+
+module.exports = router;
