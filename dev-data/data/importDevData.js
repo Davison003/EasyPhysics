5b39b852bb1fa8dad0b41edc3c110bc482fc8f29
Davi <daviregonatti@gmail.com>
2024-04-07 03:07:31 -0300

    feat: script to delete and import to db
    created script to delete all from and import all to database

---
+++ dev-data/data/importDevData.js
@@ -0,0 +1,54 @@
+const fs = require("fs");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+const Exercise = require("../../models/exerciseModel");
+
+dotenv.config({ path: "./config.env" });
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+async function dbConnect() {
+  await mongoose.connect(DB).then(() => {
+    console.log("DB connection successful!");
+  });
+}
+
+dbConnect().catch((error) => {
+  console.log(error);
+});
+
+/// Reading json file
+const exercises = JSON.parse(
+  fs.readFileSync(`${__dirname}/exercises.json`, "utf-8")
+);
+
+// Importing data to DB
+const importData = async () => {
+  try {
+    await Exercise.create(exercises);
+    console.log("Data loaded to db");
+    process.exit();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+//Delete all data from Collection
+const deleteData = async () => {
+  try {
+    await Exercise.deleteMany();
+    process.exit();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+if (process.argv[2] === "--import") {
+  importData();
+} else if (process.argv[2] === "--delete") {
+  deleteData();
+}
