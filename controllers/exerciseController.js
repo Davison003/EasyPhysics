941b69289c09ebdf12480a6b94029f518e6ac693
Davi <daviregonatti@gmail.com>
2024-04-12 19:02:15 -0300

    feat: added better error handling for api
    added better options for error handling in the api routes

--- controllers/exerciseController.js
+++ controllers/exerciseController.js
@@ -1,23 +1,24 @@
 const Exercise = require("../models/exerciseModel");
+const APIFeatures = require("../utils/apiFeatures");
+const catchAsync = require("../utils/catchAsync");
 
-exports.getAllExercises = async (req, res) => {
-  try {
-    const exercises = await Exercise.find();
+exports.getAllExercises = catchAsync(async (req, res, next) => {
+  const features = new APIFeatures(Exercise.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
 
-    res.status(200).json({
-      status: "success",
-      results: exercises.length,
-      data: {
-        exercises: exercises,
-      },
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: "fail",
-      message: err,
-    });
-  }
-};
+  const exercises = await features.query;
+
+  res.status(200).json({
+    status: "success",
+    results: exercises.length,
+    data: {
+      exercises: exercises,
+    },
+  });
+});
 
 exports.getExercise = async (req, res) => {
   try {
