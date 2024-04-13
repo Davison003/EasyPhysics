b3ea21bd4c26e46fee6c84632646431aca1a27cd
Davi <daviregonatti@gmail.com>
2024-04-13 01:02:08 -0300

    feat: added funcs to sor by difficulty
    added functions that send to other urls

--- public/js/front.js
+++ public/js/front.js
@@ -1,41 +1,10 @@
 "use strict";
-const fs = require("fs");
 const exerciseController = require("../../controllers/exerciseController");
-const replaceTemplate = require("./replaceTemplate");
 
-const tempHome = fs.readFileSync(
-  `${__dirname}/../public/ProtoFront.html`,
-  "utf-8"
-);
+function sortByDifficultyAsc() {
+  window.location.assign("/exercises/sort=difficulty");
+}
 
-const tempCard = fs.readFileSync(`${__dirname}/../public/card.html`, "utf-8");
-
-const ascBt = document.getElementById("ascBt");
-ascBt.addEventListener("click", () => {
-  fetch("/api/exercises/?sort=difficulty,name", {
-    method: "GET",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    // body: JSON.stringify({}),
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-    });
-});
-
-document.body.onload = () => {
-  //   const exerciseList = document.getElementById("problemas-container");
-  exerciseController.getAllExercises().then((data) => {
-    console.log(data);
-
-    const cardsHtml = data.map((el) => replaceTemplate(tempCard, el).join(""));
-    const output = tempHome.replaceTemplate("{%EXERCISE_CARDS%}", cardsHtml);
-
-    // data.forEach((exercise) => {
-    //   exerciseList.innerHTML += replaceTemplate(tempCard, exercise);
-    // });
-  });
-};
+function sortByDifficultyDesc() {
+  window.location.assign("/exercises/sort=-difficulty");
+}
