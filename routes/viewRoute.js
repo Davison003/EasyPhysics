3b0067723e8c89e65d3ed50cc28f24fbb54f283b
Davi <daviregonatti@gmail.com>
2024-04-27 01:23:13 -0300

    update: removed temp funcs
    removed temporary functions, right now file render only the html

--- routes/viewRoute.js
+++ routes/viewRoute.js
@@ -1,20 +1,26 @@
 const express = require("express");
 const viewsController = require("../controllers/viewsController");
+const fs = require("fs");
 
 const router = express.Router();
 
-router.get("/", viewsController.getOverview);
+router.get("/", (req, res) => {
+  const overview = fs.readFileSync("../public/index.html", "utf8");
 
-router.get(
-  "/exercises/sort=difficulty",
-  viewsController.sortByDifficultyAsc,
-  viewsController.getOverview
-);
+  res.send(overview);
+});
+// router.post("/", viewsController.getOverview);
 
-router.get(
-  "/exercises/sort=-difficulty",
-  viewsController.sortByDifficultyDesc,
-  viewsController.getOverview
-);
+// router.get(
+//   "/exercises/sort=difficulty",
+//   viewsController.sortByDifficultyAsc,
+//   viewsController.getOverview
+// );
+
+// router.get(
+//   "/exercises/sort=-difficulty",
+//   viewsController.sortByDifficultyDesc,
+//   viewsController.getOverview
+// );
 
 module.exports = router;
